fa61636a0ff1d305cd08af75ac6d2d688d9af3ac
Chris Rocha <chris.rocha@milespartnership.com>
2021-06-05 11:05:28 -0400

    ddev use mysql, add ddev database.php connection

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+
+    // try {
+    //     DB::connection()->getPdo();
+    //     echo "Connected successfully to: " . DB::connection()->getDatabaseName();
+    // } catch (\Exception $e) {
+    //     die("Could not connect to the database. Please check your configuration. error:" . $e );
+    // }
+
     return view('welcome');
 });
