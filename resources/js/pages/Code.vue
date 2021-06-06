f60334059ed1eea0ccc12a10b010961e73e06c70
Chris Rocha <chris.rocha@milespartnership.com>
2021-06-06 01:35:22 -0400

    add fetchCode method

--- resources/js/pages/Code.vue
+++ resources/js/pages/Code.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="inset">
     <ul class="two">
-      <!-- <li v-for="c in code">
+      <li v-for="(c, i) in code" :key="i">
       <a target="_blank" :href="c.link" :title="c.title">
         <img loading="lazy" width="300" height="240" :alt="c.title" class="scale-with-grid" :src="c.thumb" />
       <div class="caption">
@@ -9,16 +9,37 @@
       <div class="details">{{ c.details}}</div>
       </div>
       </a>
-      </li> -->
+      </li>
     </ul>
 </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     name: "Code",
-    // data: function() {
-    //     return { code: Settings.code }
-    // }
+    data: function() {
+        return {
+            code: []
+        }
+    },
+    mounted() {
+        this.fetchCode();
+    },
+    methods: {
+        async fetchCode() {
+            try {
+                const code = await axios.get('/api/code');
+                this.code = code.data;
+            }
+            catch (e) {
+                console.log(e);
+            }
+            finally {
+                console.log(this.code);
+            }
+        },
+    }
 };
 </script>
