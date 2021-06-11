b376a438e00ff74faa61bb6020c2256cbb7f13c2
Chris Rocha <chris.rocha@milespartnership.com>
2021-06-10 20:34:35 -0400

    update admin form to store design to db

--- app/Http/Controllers/Web/AdminController.php
+++ app/Http/Controllers/Web/AdminController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Web;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use App\Models\Design;
+
 
 class AdminController extends Controller
 {
@@ -21,11 +23,30 @@ class AdminController extends Controller
     public function upload(Request $request)
     {
         $request->validate([
-            'file' => 'required|mimes:jpg,jpeg|max:2048'
+            'file' => 'required|mimes:png,jpg,jpeg|max:2048'
         ]);
 
-        return back()
-        ->with('success','File has been uploaded.');
+        $file_model = new Design;
+
+
+        if($request->file()) {
+            $file_model->type = $request->type;
+
+
+            $file_name = time() . '_' . $request->file->getClientOriginalName();
+            $file_path = $request->file('file')->storeAs('uploads', $file_name, 'public');
+
+            // dd($file_model);
+
+            // $file_model->name = time() . '_' . $request->file->getClientOriginalName();
+            $file_model->thumb = '/storage/' . $file_path;
+            $file_model->save();
+
+
+            return back()
+            ->with('success','File has been uploaded.')
+            ->with('file', $file_name);
+        }
     }
 
 }
